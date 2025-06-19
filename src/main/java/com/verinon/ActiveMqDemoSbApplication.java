5fb091afaf7af5c9f04bf30f9f82bab8e2378dbc
Mekala Rajkumar <rajkumarmekala1@gmail.com>
2025-06-19 08:47:12 +0530

    Just added comment in main class

--- src/main/java/com/verinon/ActiveMqDemoSbApplication.java
+++ src/main/java/com/verinon/ActiveMqDemoSbApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ActiveMqDemoSbApplication {
-
+	//this is spring boot main start class
 	public static void main(String[] args) {
 		SpringApplication.run(ActiveMqDemoSbApplication.class, args);
 	}
